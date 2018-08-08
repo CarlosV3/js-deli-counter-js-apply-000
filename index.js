b39e9f4467d82028c81f3e0714601063bb176d0b
Carlos Vallejo <carlos.v3@outlook.com>
2018-08-08 14:47:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,10 +31,16 @@ function currentLine(line) {
   }
 }
 
-takeANumber(line, "Humzah") => hello humzah you are number1 in line
-takeANumber(line, "joe") => hello humzah you are number2 in line
-nowServing
-takeANumber => hello jim you are number 3 in line 
-function takeANumber() 
+//takeANumber(line, "Humzah") => hello humzah you are number1 in line
+//takeANumber(line, "joe") => hello humzah you are number2 in line
+//nowServing
+//takeANumber => hello jim you are number 3 in line 
+
+function takeANumber() {
+  var number = 0;
+  number++
+  return `You are number ${number}`
+}
+
 
 
